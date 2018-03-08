f9270c022591789b0919d6a3cbfd5666f15d04ae
jpcrs <joaop.carrasco@gmail.com>
2018-03-08 13:53:34 -0300

    commentmodel

--- src/Interfaces/ModelsInterface.ts
+++ src/Interfaces/ModelsInterface.ts
@@ -1,7 +1,9 @@
 import { UserModel } from "../Models/UserModel";
 import { PostModel } from "../Models/PostModel";
+import { CommentModel } from "../Models/CommentModel";
 
 export interface ModelsInterface {
     User: UserModel;
     Post: PostModel;
+    Comment: CommentModel;
 }
