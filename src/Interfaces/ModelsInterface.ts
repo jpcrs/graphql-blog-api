0490087c09e2133c7488137bf1bc2099ecac78fb
jpcrs <joaop.carrasco@gmail.com>
2018-03-08 13:43:33 -0300

    models and postmodel

--- src/Interfaces/ModelsInterface.ts
+++ src/Interfaces/ModelsInterface.ts
@@ -1,3 +1,7 @@
+import { UserModel } from "../Models/UserModel";
+import { PostModel } from "../Models/PostModel";
+
 export interface ModelsInterface {
-    
+    User: UserModel;
+    Post: PostModel;
 }
